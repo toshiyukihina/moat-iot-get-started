53d0502b37730f65439af3cee2a2ffb0534fbb5d
Daisuke Baba <dbaba@yourinventit.com>
2013-01-05 02:29:44 +0900

    Removes unused app.

--- app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-App::Application.config.secret_token = 'feb8587fec85a78c6680cea72dad66f0b8715a275673ee09dc42f5a3088bf26e2b87db9645fc78d670a9de932e490a9241b1741b1e87aa2295986c357500cb08'
