a6a56b66703f483df9e73159438dd0258b87ca9c
Daisuke Baba <dbaba@yourinventit.com>
2013-04-14 22:55:41 +0900

    Adds new js files for Android-Arduino example app. Fixes an issue in ShakeEvent.js where it is not possible to update existing data in the cloud database. Adds a new data model, ZigBeeDevice, to package.json.

---
+++ simple-example-js/simple-example/InquireTemperature!1.0.js
@@ -0,0 +1,32 @@
+/*
+ * JobServiceID:
+ * urn:moat:{:your_application_id}:example:InquireTemperature:1.0
+ * 
+ * Description: Show text on the LCD
+ */
+
+var moat = require('moat');
+var context = moat.init();
+var session = context.session;
+
+session.log('inquireTemperature', 'Start InquireTemperature!');
+
+// Run ZigBeeDevice#inquireTemperature() method on the remote device!
+var mapper = session.newModelMapperStub('ZigBeeDevice');
+var device = mapper.newModelStub();
+device.inquireTemperature(session, null, {
+	success: function(obj) {
+		session.log('inquireTemperature', 'Successful!!');
+	},
+	error: function(type, code) {
+		session.log('inquireTemperature:error', 'Type:' + type
+			+ ', Unexpected status => ' + code);
+		session.notifyAsync({
+			result: 'error',
+			code: code,
+			type: type
+		});
+	}
+});
+
+session.log('inquireTemperature', 'End InquireTemperature!');
